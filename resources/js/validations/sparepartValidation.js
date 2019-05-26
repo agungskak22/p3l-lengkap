edc26c06874dee6c30bc43055abf55b13125af63
agungskak22 <agungskak22@gmail.com>
2019-05-27 03:27:50 +0700

    report and transaction handler bug

--- resources/js/validations/sparepartValidation.js
+++ resources/js/validations/sparepartValidation.js
@@ -1,24 +1,40 @@
 import { 
-    required,
-    numeric
-  } from 'vuelidate/lib/validators'
-  
-  export default {
-    form: {
-      id_sparepart : { required,
-          
-      },
-      sparepart_name: { required },
-      merk: { required },
-      id_sparepart_type: { required },
-      stock: { required,numeric },
-      min_stock: { required,numeric },
-      purchase_price: { required,numeric },
-      sell_price: { required,numeric },
-      position: { required },
-      place: { required },
-      number: { required, numeric },
-      id_motorcycle_type: {},
-      id_motorcycle_brand: {}
-    }
+  required,
+  numeric
+} from 'vuelidate/lib/validators'
+
+function priceMinimal(){
+  if(Number(this.form.sell_price)<Number(this.form.purchase_price))
+  {
+      return false
   }
+  return true
+}
+
+function stockMinimal(){
+  if(Number(this.form.stock)<Number(this.form.min_stock))
+  {
+      return false
+  }
+  return true
+}
+
+export default {
+  form: {
+    id_sparepart : { required,
+        
+    },
+    sparepart_name: { required },
+    merk: { required },
+    id_sparepart_type: { required },
+    stock: { required,numeric,stockMinimal },
+    min_stock: { required,numeric },
+    purchase_price: { required,numeric },
+    sell_price: { required,numeric,priceMinimal },
+    position: { required },
+    place: { required },
+    number: { required, numeric },
+    id_motorcycle_type: {},
+    id_motorcycle_brand: {}
+  }
+}
--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,6 @@ Route::resource('branches','BranchController');
 Route::resource('roles','RoleController');
 Route::resource('users','UserController');
 Route::resource('employees','EmployeeController');
-
 Route::resource('sales','SalesController');
 Route::get('suppliersales/{id}','SalesController@showbySupplier');
 Route::resource('suppliers','SupplierController');
@@ -31,7 +30,6 @@ Route::resource('spareparts','SparepartController');
 Route::post('updatesparepart/{id}','SparepartController@updateSparepart');
 Route::post('updatesparepart','SparepartController@sparepartVerification');
 Route::put('updatesparepartmobile/{id}','SparepartController@sparepartVerificationMobile');
-
 Route::resource('customers','CustomerController');
 Route::resource('motorcycle_brands','Motorcycle_brandController');
 Route::resource('motorcycle_types','Motorcycle_typeController');
@@ -39,45 +37,38 @@ Route::resource('motorcycles','MotorcycleController');
 Route::get('usermotorcycles/{id}','MotorcycleController@showByUser');
 Route::resource('procurements','ProcurementController');
 Route::post('procurementDetails','ProcurementController@storeDetail');
-
 Route::put('procurementDetails/{id}','ProcurementController@updateDetail');
 Route::get('procurementDetails/{id}','ProcurementController@showdetail');
 Route::resource('transactions','TransactionController');
-
 Route::post('detailService','TransactionController@storeDetailService');
 Route::get('detailService/{id}','TransactionController@showDetailService');
-
 Route::post('detailSparepart','TransactionController@storeDetailSparepart');
 Route::get('detailSparepart/{id}','TransactionController@showDetailSparepart');
-
 Route::put('payment/{id}','TransactionController@payment');
 Route::post('searchDetail','TransactionController@searchDetailService');
 Route::get('/generate-procurement-docs/{id}', 'FileController@generateProcurementDocs');
 Route::get('/generate-work-order-docs/{id}', 'FileController@generateWorkOrderDocs');
 Route::get('/generate-transaction-per-month/{year}', 'FileController@generateTransactionPerMonth');
 Route::get('/generate-sparepart-best-seller', 'FileController@generateSparepartBestSeller');
-
 Route::get('/generate-service-selling/{year}/{month}', 'FileController@generateServiceSelling');
-Route::get('/generate-sparepart-selling/{year}/{month}', 'FileController@generateSparepartSelling');
 Route::get('/generate-remaining-stock/{year}/{sparepart}', 'FileController@generateRemainingStock');
 Route::get('/chart-remaining-stock', 'FileController@chartRemainingStock');
 Route::get('/generate-transaction-per-year', 'FileController@generateTransactionPerYear');
 Route::get('/generate-expense/{year}','FileController@generateExpenseReport');
 Route::get('/generate-receipt/{id}','FileController@generateReceipt');
 Route::get('/transaction-per-year/{year}','ReportController@TransactionperYear');
-
 Route::get('/expense-per-year/{year}','ReportController@ExpenseperYear');
 Route::get('/transaction-by-branch','ReportController@TransactionbyBranch');
 Route::get('/best-seller-sparepart','ReportController@BestSellerSparepart');
 Route::get('/service-selling/{year}/{month}','ReportController@ServiceSelling');
-Route::get('/sparepart-selling/{year}/{month}','ReportController@SparepartSelling');
 Route::get('/remaining-stock/{year}/{sparepart}','ReportController@RemainingStock');
 Route::get('/detail-sparepart/{id}','ReportController@DetailSparepart');
 Route::get('/detail-service/{id}','ReportController@DetailService');
-
 Route::get('/work-order/{id}','ReportController@WorkOrder');
 Route::get('/customer-service/{id}','ReportController@DataCS');
 Route::get('/mechanic-detail-sparepart/{id}','ReportController@MechanicSP');
 Route::get('/mechanic-detail-service/{id}','ReportController@MechanicSV');
 Route::get('/motor-sparepart/{id}','ReportController@MotorSparepart');
 Route::get('/motor-service/{id}','ReportController@MotorService');
+Route::get('/generate-spk/{id}','ReportController@cetaksuratperintahkerja');
+Route::get('/generate-sp/{id}','ReportController@CetakPemesanan');
